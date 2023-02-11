4f11eeeb09f5e3821fb26f36f00e75021bafa3d6
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-12 01:26:04 +0900

    section5

--- section/section5/section5.js
+++ section/section5/section5.js
@@ -1,3 +1,10 @@
-const score = null;
+function score (score) {
+  if (score >= 80 && score <= 100) return "合格";
+  else if (score >= 60 && score < 80) return "追試";
+  else if (score >= 0 && score < 60) return "不合格";
+  else if (score == undefined) return "後日受験"; 
+  else if (score < 0 || score > 100 ) return "0以上100未満の数字を入れてください";
+  else if (typeof score !== 'number' ) return "0以上100未満の数字を入れてください";
+  }
 
 module.exports = { score };
