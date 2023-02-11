346a67e4018f780ee1a1f58269cd35cd2d6c1f28
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-12 01:10:35 +0900

    section4

--- section/section4/section4.js
+++ section/section4/section4.js
@@ -1,5 +1,12 @@
-const echoDiv = null;
+function echoDiv (num1, num2) {
+  let shou = Math.floor(num1 / num2);
+  let mod = Math.floor(num1 % num2);
+  return "商: " + shou + ", 余り: " + mod;
+}
 
-const reply = null;
+function reply (reply) {
+  if (reply === undefined) return "返信なし";
+  else                     return reply;
+}
 
 module.exports = { echoDiv, reply };
