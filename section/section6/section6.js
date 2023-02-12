5f07351cef12826410c0f318850980d3c7e1d436
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-12 10:19:35 +0900

    section6

--- section/section6/section6.js
+++ section/section6/section6.js
@@ -1,3 +1,18 @@
-const sum = null;
+function sum (from, to) {
+  let sum = 0;
+  if (from < to) {
+    for (let i = from; i <= to ; i++) {
+      sum += i
+    }
+  }
+  else {
+    for (let i = from; i >= to ; i--) {
+      sum += i
+    }
+  }
+
+  return sum;
+}
+
 
 module.exports = { sum };
